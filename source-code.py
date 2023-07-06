83440bc30df462c119fef81c365b785e3be2503d
Adam Abdulkareem <dmabdulkareem@gmail.com>
2023-07-06 12:09:59 +0100

    Update source-code.py

--- source-code.py
+++ source-code.py
@@ -9,38 +9,48 @@ import datetime
 
 
 def extract_product_names():
-    URL = "https://www.amazon.com/s?k=macbook&i=electronics&rh=n%3A172282%2Cp_89%3AApple%2Cp_n_availability%3A2661601011&dc&crid=RVOV47UY4KOG&qid=1688578107&rnid=2661599011&sprefix=macbook%27%2Caps%2C365&ref=sr_pg_1"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Upgrade-Insecure-Requests": "1",
-    }
+    for num in range(1, 12):
+        URL = (
+            "https://www.amazon.com/s?k=macbook&i=electronics&rh=n%3A172282%2Cp_89%3AApple%2Cp_n_availability%3A2661601011&page="
+            + str(num)
+        )
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Upgrade-Insecure-Requests": "1",
+        }
 
-    try:
-        page = requests.get(URL, headers=headers)
-        page.raise_for_status()
+        try:
+            page = requests.get(URL, headers=headers)
+            page.raise_for_status()
 
-        soup = BeautifulSoup(page.content, "html.parser")
+            soup = BeautifulSoup(page.content, "html.parser")
 
-        title_elements = soup.select("[data-component-type='s-product-image'] img")
-        
-        product_names = []
-        
-        for title_element in title_elements:
-            product_name = title_element.get("alt")
-            product_names.append(product_name)
+            title_elements = soup.select("[data-component-type='s-product-image'] img")
+            id_elements = soup.select("[data-asin][data-component-type='s-search-result']")
+            
+            file_path = "./Amazon_Web_Dataset.csv"
+            
+            if not os.path.exists(file_path):
+                header = ["Title", "ID"] 
+                with open("Amazon_Web_Dataset.csv", mode="w", newline="", encoding="utf-8") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(header)
+            
+            for title_element, element in zip(title_elements, id_elements):
+                product_name = title_element.get("alt")
+                asin = element["data-asin"]
+                data = [product_name, asin]
 
-        return product_names
-
-    except requests.RequestException as e:
-        return f"An error occurred: {type(e).__name__} - {str(e)}"
-
+                with open("Amazon_Web_Dataset.csv", mode="a+", newline="", encoding="utf-8") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(data)
+            
+        except requests.RequestException as e:
+            return f"An error occurred: {type(e).__name__} - {str(e)}"
 
 extract_product_names()
-
-
-
 
 
 # def check_price():
@@ -76,7 +86,7 @@ extract_product_names()
 #     header = ["Title", "Price", "Rating", "Number of reviews", "Date"]
 #     data = [title, price, ratings, num_reviews, today]
 
-    
+
 #     file_path = "./AmazonWebDataset.csv"
 #     if os.path.exists(file_path):
 #         # Now we are appending data to the csv file
@@ -94,4 +104,3 @@ extract_product_names()
 # while(True):
 #     check_price()
 #     time.sleep(10)
-
